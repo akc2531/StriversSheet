415892d8c895d6be23c64655f85104f5a5eb60a9
akc2531 <117503109+akc2531@users.noreply.github.com>
2023-10-19 23:36:01 +0530

    Create ifelse_comparison.java

---
+++ ifelse_comparison.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static String compareIfElse(int a, int b) {
+        // Write your code here
+        if(a>b)
+        return "greater";
+        else if(a<b)
+        return "smaller";
+        return "equal";
+
+    }
+}public class Solution {
+    public static String compareIfElse(int a, int b) {
+        // Write your code here
+        if(a>b)
+        return "greater";
+        else if(a<b)
+        return "smaller";
+        return "equal";
+
+    }
+}
