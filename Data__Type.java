9326c1dc5f39c6d9dc4b39c6a0defc060bf7d9d4
akc2531 <117503109+akc2531@users.noreply.github.com>
2023-10-19 23:32:11 +0530

    Create Data__Type.java

---
+++ Data__Type.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public static int dataTypes(String type) {
+        // Write your code here
+        if(type.equals("Integer"))
+        return 4;
+         else if(type.equals("Long"))
+         return 8;
+         else if(type.equals("Float"))
+         return 4;
+         else if(type.equals("Double"))
+         return 8;
+         else if(type.equals("Character"))
+         return 1;
+    return 0;
+    }
+}
