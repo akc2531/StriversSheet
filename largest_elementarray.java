25646eac7c82a92221febfc6308aabfd61e224d1
akc2531 <117503109+akc2531@users.noreply.github.com>
2023-10-21 23:43:39 +0530

    Create largest_elementarray.java

---
+++ largest_elementarray.java
@@ -0,0 +1,16 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+    static int largestElement(int[] arr, int n) {
+        // Write your code here.
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            max=arr[i];
+        }
+       return max;
+    }
+}
